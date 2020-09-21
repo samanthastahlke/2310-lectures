52fdfda6753e1e1f14bf324091dbc2ace3016b53
samanthastahlke <samanthastahlke@gmail.com>
2020-09-21 11:44:05 -0400

    Added completed code and Unity scene for Module 3 (Basic Systems)

---
+++ 2310-Lecture-Demos/Assets/Module 3 - Basic Systems/Completed/Scripts/WarriorController_COMPLETED.cs
@@ -0,0 +1,86 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+using UnityEngine;
+using StateUtility;
+
+//If we added this controller to an object,
+//this makes it so Unity would automatically add a SimpleSpriteAnimator.
+[RequireComponent(typeof(SimpleSpriteAnimator))]
+public class WarriorController_COMPLETED : MonoBehaviour
+{
+    //These are just row indices for each animation in the spritesheet
+    //that we will be using.
+    public int idleID = 0;
+    public int walkID = 2;
+    public int attackID = 3;
+    public int deathID = 4;
+
+    public float walkSpeed = 2.0f;
+
+    private SimpleSpriteAnimator animator;
+    private SimpleAnimFSM fsm;
+
+    void Start()
+    {
+        animator = GetComponent<SimpleSpriteAnimator>();
+        fsm = new SimpleAnimFSM();
+
+        //Add our triggers to the FSM.
+        fsm.AddTrigger(Warrior.Keywords.deathTrigger);
+        fsm.AddTrigger(Warrior.Keywords.attackTrigger);
+        fsm.AddTrigger(Warrior.Keywords.respawnTrigger);
+
+        //Add our persistent variables to the FSM.
+        fsm.AddVar(Warrior.Keywords.canMove, true);
+        fsm.AddVar(Warrior.Keywords.moveInput, false);
+
+        //Add states to the FSM.
+        fsm.AddState(StateID.IDLE_STATE, new IdleState_COMPLETED(idleID, animator, fsm));
+        fsm.AddState(StateID.WALK_STATE, new WalkState_COMPLETED(walkID, animator, fsm));
+        fsm.AddState(StateID.ATTACK_STATE, new AttackState_COMPLETED(attackID, animator, fsm));
+        fsm.AddState(StateID.DIE_STATE, new DieState_COMPLETED(deathID, animator, fsm));
+
+        //Initialize FSM state.
+        fsm.SetState(StateID.IDLE_STATE);
+    }
+
+    void Update()
+    {
+        //Get horizontal input from Unity's Input manager.
+        float inputAxis = Input.GetAxis("Horizontal");
+        fsm.SetVar(Warrior.Keywords.moveInput, inputAxis != 0.0f);
+
+        if(fsm.GetVar(Warrior.Keywords.canMove) && inputAxis != 0.0f)
+        {
+            //Flip our sprite renderer if necessary.
+            bool faceLeft = inputAxis < 0;
+            animator.spriteRenderer.flipX = faceLeft;
+
+            //Update position based on walk speed.
+            transform.position += inputAxis * Time.deltaTime * walkSpeed * Vector3.right;
+        }
+
+        //Attack button trigger.
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            fsm.SetTrigger(Warrior.Keywords.attackTrigger);
+        }
+
+        //Respawn button trigger.
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            fsm.SetTrigger(Warrior.Keywords.respawnTrigger);
+        }
+
+        //Update our FSM - it will handle all the animation legwork.
+        fsm.Update();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Warrior.Keywords.deathTag))
+        {
+            fsm.SetTrigger(Warrior.Keywords.deathTrigger);
+        }        
+    }
+}
---
+++ 2310-Lecture-Demos/Assets/Module 3 - Basic Systems/Completed/Scripts/WarriorStates_COMPLETED.cs
@@ -0,0 +1,114 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+
+//Custom states for the Warrior controller.
+//State transitions follow the logic we discussed during class.
+//Basically:
+//- The death animation can interrupt anything
+//- Player is frozen during the attack animation
+//- If player is moving, then play walk
+//- Default unmoving animation is idle
+namespace StateUtility
+{
+    public class IdleState_COMPLETED : AnimState
+    {
+        public IdleState_COMPLETED(int p_clipID, SimpleSpriteAnimator p_anim, SimpleAnimFSM p_fsm)
+                  : base(p_clipID, p_anim, p_fsm) { }
+
+        public override void OnEnter()
+        {
+            anim.PlayLoop(clipID);
+        }
+        public override void Update()
+        {
+            if (fsm.GetTrigger(Warrior.Keywords.deathTrigger))
+                fsm.SetState(StateID.DIE_STATE);
+
+            else if (fsm.GetTrigger(Warrior.Keywords.attackTrigger))
+                fsm.SetState(StateID.ATTACK_STATE);
+
+            else if (fsm.GetVar(Warrior.Keywords.moveInput))
+                fsm.SetState(StateID.WALK_STATE);
+        }
+    }
+
+    public class WalkState_COMPLETED : AnimState
+    {
+        public WalkState_COMPLETED(int p_clipID, SimpleSpriteAnimator p_anim, SimpleAnimFSM p_fsm)
+                  : base(p_clipID, p_anim, p_fsm) { }
+
+        public override void OnEnter()
+        {
+            anim.PlayLoop(clipID);
+        }
+
+        public override void Update()
+        {
+            if (fsm.GetTrigger(Warrior.Keywords.deathTrigger))
+                fsm.SetState(StateID.DIE_STATE);
+
+            else if (fsm.GetTrigger(Warrior.Keywords.attackTrigger))
+                fsm.SetState(StateID.ATTACK_STATE);
+
+            else if (!fsm.GetVar(Warrior.Keywords.moveInput))
+                fsm.SetState(StateID.IDLE_STATE);
+        }
+
+    }
+
+    public class AttackState_COMPLETED : AnimState
+    {
+        public AttackState_COMPLETED(int p_clipID, SimpleSpriteAnimator p_anim, SimpleAnimFSM p_fsm)
+                    : base(p_clipID, p_anim, p_fsm) { }
+
+        public override void OnEnter()
+        {
+            anim.PlayOnce(clipID);
+            fsm.SetVar(Warrior.Keywords.canMove, false);
+        }
+
+        public override void Update()
+        {
+            if (fsm.GetVar("deathEvent"))
+                fsm.SetState(StateID.DIE_STATE);
+
+            if (anim.IsDone())
+            {
+                if (fsm.GetVar(Warrior.Keywords.moveInput))
+                    fsm.SetState(StateID.WALK_STATE);
+                else
+                    fsm.SetState(StateID.IDLE_STATE);
+            }
+        }
+
+        public override void OnExit()
+        {
+            fsm.SetVar(Warrior.Keywords.canMove, true);
+        }
+    }
+
+    public class DieState_COMPLETED : AnimState
+    {
+        public DieState_COMPLETED(int p_clipID, SimpleSpriteAnimator p_anim, SimpleAnimFSM p_fsm)
+                : base(p_clipID, p_anim, p_fsm) { }
+
+        public override void OnEnter()
+        {
+            anim.PlayOnce(clipID);
+            fsm.SetVar(Warrior.Keywords.canMove, false);
+        }
+
+        public override void Update()
+        {
+            if (anim.IsDone() && fsm.GetTrigger(Warrior.Keywords.respawnTrigger))
+            {
+                fsm.SetState(StateID.IDLE_STATE);
+            }
+        }
+
+        public override void OnExit()
+        {
+            fsm.SetVar(Warrior.Keywords.canMove, true);
+        }
+    }
+}
