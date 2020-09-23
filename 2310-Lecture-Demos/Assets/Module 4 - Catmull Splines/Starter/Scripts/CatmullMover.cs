84afb61a4b72657307a4d4b59f787527cd3eb6f1
samanthastahlke <samanthastahlke@gmail.com>
2020-09-23 13:24:27 -0400

    Added starter code and assets for Module 4 (Catmull-Rom splines)

---
+++ 2310-Lecture-Demos/Assets/Module 4 - Catmull Splines/Starter/Scripts/CatmullMover.cs
@@ -0,0 +1,48 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatmullDemo
+{
+    public static Vector3 Catmull(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        //TODO: Implement Catmull-Rom interpolation.
+        return Vector3.Lerp(p1, p2, t);
+    }
+}
+
+public class CatmullMover : MonoBehaviour
+{
+    //List of waypoints.
+    public List<Transform> points;
+
+    //How much time should each segment take?
+    //...We can't control our speed directly yet, that will come in another week!
+    public float segmentTime;
+
+    private int currentIndex = 0;
+    private float t;
+
+    void Start()
+    {
+        if (segmentTime <= 0)
+            segmentTime = 1.0f;
+
+        //TODO: Initialize our position to what?
+    }
+
+    void Update()
+    {
+        //It only makes sense for us to do Catmull-Rom interpolation with at least 4 points. 
+        if (points.Count >= 4)
+        {
+            //TODO: What's our update look like?
+        }
+    }
+
+    void StartSegment(int startIndex)
+    {
+        //TODO: Complete this function.
+    }
+}
---
+++ 2310-Lecture-Demos/Assets/Module 4 - Catmull Splines/Starter/Scripts/Editor/CatmullEditor.cs
@@ -0,0 +1,30 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+using UnityEditor;
+
+//You don't need to worry about understanding this file for this class.
+//If you are curious:
+//It hooks into Unity's scene GUI update and calls some custom utilities to
+//draw a path in the Editor scene.
+[CustomEditor(typeof(CatmullMover))]
+public class CatmullEditor : Editor
+{
+    private const int samples = 8;
+
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += GUICallback;
+    }
+
+    private void GUICallback(SceneView sceneview)
+    {
+        CatmullMover obj = target as CatmullMover;
+
+        if (obj == null)
+            return;
+
+        PathDrawUtility.DrawCatmullPath(obj.points,
+                                        CatmullDemo.Catmull,
+                                        samples);
+    }
+}
