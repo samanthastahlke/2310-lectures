86314e75aa2f0bf39089d54777c8fc10e102ceed
samanthastahlke <samanthastahlke@gmail.com>
2020-10-23 15:30:32 -0400

    Added starter scene and assets for Module 11 (Blending/Transitions)

---
+++ 2310-Lecture-Demos/Assets/Module 11 - Blending/Starter/Scripts/AnimatorUIControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorUIControls : MonoBehaviour
+{
+    public Animator controller;
+    public Toggle moveToggle;
+    public Slider blendSlider;
+
+    void Update()
+    {
+        controller.SetBool("Moving", moveToggle.isOn);
+        controller.SetFloat("WalkRunBlend", blendSlider.value);
+    }
+}
