4ae149e5a00dfedd0747e72b5ef27a12c5a6bab8
samanthastahlke <samanthastahlke@gmail.com>
2020-10-29 08:59:53 -0400

    Added starter code and assets for Module 12 (Steering/Boids)

---
+++ 2310-Lecture-Demos/Assets/Module 12 - Boids/Starter/Scripts/Boid.cs
@@ -0,0 +1,97 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    private Flock flock;
+    private Vector3 velocity;
+
+    //The flock script will call this when the boid is spawned.
+    public void Init(Flock p_flock)
+    {
+        flock = p_flock;
+
+        //Random position within flock's spawn radius.
+        transform.position = Random.Range(0.0f, flock.spawnRadius) * Random.onUnitSphere;
+        //Random velocity within min and max speed range.
+        velocity = Random.Range(flock.minSpeed, flock.maxSpeed) * Random.onUnitSphere;
+    }
+
+    //The flock script will call this every frame to update the boid.
+    public void BoidUpdate(List<Boid> boids)
+    {
+        int separationCount = 0;
+        int neighbourCount = 0;
+
+        //Used to calculate average position of flockmates for cohesion.
+        Vector3 sumPos = Vector3.zero;
+        //Used to calculate average heading of flockmates for alignment.
+        Vector3 sumHeading = Vector3.zero;
+        //Used to calculate separation.
+        Vector3 sumDiffs = Vector3.zero;
+
+        foreach (Boid boid in boids)
+        {
+            //TODO: Check if this boid is a "neighbour" and skip if not.
+
+            //TODO: Add to position sum.
+
+            //TODO: Add to heading sum.
+
+            //TODO: Add to separation calculation.
+        }
+
+        //Calculate forces...
+        Vector3 cohesion = Vector3.zero;
+        Vector3 alignment = Vector3.zero;
+        Vector3 separation = Vector3.zero;
+
+        if (neighbourCount > 0)
+        {
+            //TODO: Calculate cohesion.
+
+            //TODO: Calculate alignment.
+
+            //TODO: Calculate separation.
+            if (separationCount > 0)
+            {
+                //...
+            }
+        }
+
+        //TODO: Basic Euler integration using calculated forces.
+        //Calculate acceleration, update velocity and position.
+    }
+
+    //The flock script will call this function on the boid every frame after its main update.
+    public void ConstrainAndOrient(BoxCollider borders)
+    {
+        //Constrain position to the flock's borders via simple wrap-around.
+        Vector3 pos = transform.position;
+        Vector3 halfSize = borders.size * 0.5f;
+        Vector3 center = borders.transform.position + borders.center;
+
+        if (pos.x > center.x + halfSize.x)
+            pos.x = center.x - halfSize.x;
+        else if (pos.x < center.x - halfSize.x)
+            pos.x = center.x + halfSize.x;
+
+        if (pos.y > center.y + halfSize.y)
+            pos.y = center.y - halfSize.y;
+        else if (pos.y < center.y - halfSize.y)
+            pos.y = center.y + halfSize.y;
+
+        if (pos.z > center.z + halfSize.z)
+            pos.z = center.z - halfSize.z;
+        else if (pos.z < center.z - halfSize.z)
+            pos.z = center.z + halfSize.z;
+
+        transform.position = pos;
+
+        //Orient the boid so it is facing in the direction of its current velocity.
+        if (velocity.magnitude > 0.0f)
+            transform.LookAt(transform.position + velocity);
+    }
+}
---
+++ 2310-Lecture-Demos/Assets/Module 12 - Boids/Starter/Scripts/Flock.cs
@@ -0,0 +1,54 @@
+﻿//(c) Samantha Stahlke 2020
+//Created for INFR 2310.
+using System.Collections.Generic;
+using UnityEngine;
+
+//This will make Unity stick a box collider on anything we give the Flock component.
+[RequireComponent(typeof(BoxCollider))]
+public class Flock : MonoBehaviour
+{
+    public Boid boidPrefab;
+    public int numBoids;
+
+    //Some settings to control our flock's behaviour.
+    //Radius for how far away a boid can "see" its neighbours.
+    public float awareness = 3.0f;
+    //Radius around the origin in which new boids can spawn.
+    public float spawnRadius = 2.0f;
+    //Distance which boids will try to keep from one another at minimum.
+    public float separationDistance = 0.5f;
+    //Weighting for the different forces governing boid behaviour.
+    public float alignmentWeight = 1.0f;
+    public float cohesionWeight = 1.0f;
+    public float separationWeight = 1.0f;
+    //Maximum force (acceleration) that can be applied.
+    public float maxForce = 0.5f;
+    //Min and max speeds for boid "flight".
+    public float minSpeed = 4.0f;
+    public float maxSpeed = 16.0f;
+
+    private List<Boid> boids = new List<Boid>();
+    private BoxCollider borders;
+
+    void Start()
+    {
+        borders = GetComponent<BoxCollider>();
+
+        //Instantiate a bunch of boids.
+        for (int i = 0; i < numBoids; ++i)
+        {
+            Boid boid = Instantiate(boidPrefab) as Boid;
+            boid.Init(this);
+            boids.Add(boid);
+        }
+    }
+
+    void Update()
+    {
+        foreach (Boid boid in boids)
+        {
+            boid.BoidUpdate(boids);
+            boid.ConstrainAndOrient(borders);
+        }
+    }
+}
